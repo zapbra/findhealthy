6eb3621cb760f5bde55e3f84ce1086fc1f70dbb0
Matthew Pierce <84364656+miscany@users.noreply.github.com>
2023-01-31 15:58:32 +0000

    c

--- pages/farm/Preview.js
+++ pages/farm/Preview.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
 import ImageSection from "./ImageSection";
+import Sections from "./Sections";
 const Cont = styled.div``;
 
 const Preview = () => {
@@ -12,9 +13,55 @@ const Preview = () => {
     "/images/milk.jpg",
     "/images/farm.jpg",
   ]);
+
+  const products = [
+    {
+      name: "chicken",
+      price: "6.75",
+      dollarType: "CAD",
+      measurement: "lb",
+    },
+    {
+      name: "ribeye",
+      price: "20",
+      dollarType: "CAD",
+      measurement: "lb",
+    },
+    {
+      name: "eggs",
+      price: "7.50",
+      dollarType: "CAD",
+      measurement: "doz",
+    },
+    {
+      name: "liver",
+      price: "7",
+      dollarType: "CAD",
+      measurement: "lb",
+    },
+    {
+      name: "pancreas",
+      price: "10",
+      dollarType: "CAD",
+      measurement: "lb",
+    },
+    {
+      name: "dry aged sausages",
+      price: "",
+      dollarType: "CAD",
+      measurement: "",
+    },
+    {
+      name: "",
+      price: "",
+      dollarType: "CAD",
+      measurement: "",
+    },
+  ];
   return (
     <Cont colors={COLORS}>
-      <ImageSection images = {images} />
+      <ImageSection images={images} />
+      <Sections products={products} />
     </Cont>
   );
 };
--- pages/farm/Sections.js
+++ pages/farm/Sections.js
@@ -1,15 +1,57 @@
-import { useState} from "react"
-import styled from "styled-components"
-import COLORS from "../../data/colors"
+import { useState } from "react";
+import styled from "styled-components";
+import COLORS from "../../data/colors";
 
 const Cont = styled.div`
-
+  .section-holder {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-template-rows: 1fr 1fr;
+  }
+  .section {
+    width: 100%;
+    display: flex;
+    padding: 32px;
+    background-color: #fff;
+    &:nth-of-type(2) {
+      background-color: ${(props) => props.colors.lightBeige};
+    }
+  }
 `;
 
-const Sections = () => {
+const Sections = ({
+  products,
+  description,
+  address,
+  website,
+  email,
+  phone,
+  delivery,
+  hours,
+  howToOrder,
+  grassFed,
+  organic,
+  vaccineFree,
+  soyFree,
+  pastureRaised,
+  dewormerFree,
+  unfrozen,
+  pricing,
+  quality,
+  friendly,
+}) => {
   return (
-    <div>Sections</div>
-  )
-}
+    <Cont colors={COLORS}>
+      <section className="section">
+        <div className="center-inline">
+          <h3>PRODUCTS</h3>
+        </div>
+      </section>
+      <section className="section"></section>
+      <section className="section"></section>
+      <section className="section"></section>
+    </Cont>
+  );
+};
 
-export default Sections
+export default Sections;
