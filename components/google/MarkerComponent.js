0dd8fc60498138f87734a0f38e21b83fefc35ea7
Matthew Pierce <84364656+miscany@users.noreply.github.com>
2023-01-31 03:36:39 +0000

    c

--- components/google/MarkerComponent.js
+++ components/google/MarkerComponent.js
@@ -22,6 +22,7 @@ const MarkerComponent = ({
   console.log(latLong);
   return (
     <Marker
+    
       icon={{
         path: "M8 12l-4.7023 2.4721.898-5.236L.3916 5.5279l5.2574-.764L8 0l2.3511 4.764 5.2574.7639-3.8043 3.7082.898 5.236z",
         fillColor: "yellow",
