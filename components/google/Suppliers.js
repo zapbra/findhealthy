2339cccfe23be37c1ee7485beb202be4e1ca91e8
Matthew Pierce <84364656+miscany@users.noreply.github.com>
2023-02-01 21:49:01 +0000

    c

--- components/google/Suppliers.js
+++ components/google/Suppliers.js
@@ -2,12 +2,31 @@ import styled from "styled-components";
 import COLORS from "../../data/colors";
 import SupplierBox from "./SupplierBox";
 const Cont = styled.div`
+    .title-spec{
+        background-color: ${props=>props.colors.tan};
+        position: relative;
+       z-index:5000;
+    }
+    .slanted-bg{
+        background-color: ${props=>props.colors.tan};
+        height:200px;
+        width:100%;
+        position: absolte;
+        top: calc(-100px); 
+        z-index:-1;
 
+        transform: rotate(5deg);
+    }
 `;
 const Suppliers = () => {
   return (
-    <Cont>
+    <Cont colors = {COLORS}>
+        <div className="title-spec center-inline">
         <h2>POPULAR FOOD SUPPLIERS!</h2>
+        <div className="slanted-bg">
+
+        </div>
+        </div>
     </Cont>
   )
 }
