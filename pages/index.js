3d5041cdf41371193dc56280dab613862661435f
Matthew Pierce <84364656+miscany@users.noreply.github.com>
2023-02-03 16:02:41 +0000

    c

--- pages/index.js
+++ pages/index.js
@@ -69,6 +69,7 @@ export default function Home({ locationsFetch, tagsFetch }) {
         tagsFetch={tags}
         addTag={addTag}
         fetchNewLocation={fetchNewLocation}
+        
       />
     </Cont>
   );
