3905da572aa1be4c6a1bd7197bd2493ae5ffcc6e
Matthew Pierce <84364656+miscany@users.noreply.github.com>
2023-02-03 16:56:51 +0000

    c

--- components/google/SupplierBox.js
+++ components/google/SupplierBox.js
@@ -1,5 +1,5 @@
-import Link from 'next/link';
-import Image from 'next/image';
+import Link from "next/link";
+import Image from "next/image";
 import styled from "styled-components";
 import { useState } from "react";
 import COLORS from "../../data/colors";
@@ -9,119 +9,119 @@ import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import Aos from "aos";
 import "aos/dist/aos.css";
 const Cont = styled.div`
-  display:flex;
+  display: flex;
   text-align: center;
   align-items: center;
   word-break: break-word;
   margin-bottom: 128px;
-  @media only screen and (max-width:900px){
-      flex-direction: column;
-    }
-  & > div{
+  @media only screen and (max-width: 900px) {
+    flex-direction: column;
+  }
+  & > div {
     padding: 32px;
-    @media only screen and (max-width:900px){
+    @media only screen and (max-width: 900px) {
       padding: 16px;
     }
   }
-  li, p{
-    display:inline-block;
+  li,
+  p {
+    display: inline-block;
   }
-  li::before{
-    content: ''
+  li::before {
+    content: "";
   }
-  .lines{
-    display:flex;
+  .lines {
+    display: flex;
     justify-content: center;
     flex-direction: column;
-    align-items:center;
+    align-items: center;
   }
-  .lines{
-    & > div{
+  .lines {
+    & > div {
       margin-bottom: 8px;
     }
   }
-  li{
+  li {
     margin-bottom: 8px;
   }
-  .image-holder{
+  .image-holder {
     position: relative;
-    width:100%;
-    height:100%;
-    overflow:hidden;
-    min-height:400px;
-    img{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    min-height: 400px;
+    img {
       padding: 32px;
-      @media only screen and (max-width:900px){
-      padding: 16px;
-    }
+      @media only screen and (max-width: 900px) {
+        padding: 16px;
+      }
     }
   }
 `;
 
-const SupplierBox = ({links, pros, cons, name, country, image}) => {
+const SupplierBox = ({ links, pros, cons, name, country, image }) => {
+  const [proElems, setProElems] = useState(
+    pros.map((pro, index) => {
+      return (
+        <li key={index}>
+          <p className="bold">{pro}</p>
+        </li>
+      );
+    })
+  );
 
-  const [proElems, setProElems] = useState(pros.map(pro=> {
-    return (
-      <li>
-        <p className="bold">
-          {pro}
-        </p>
-      </li>
-    )
-  }));
-  
-  const [conElems, setConElems] = useState(cons.map(con=> {
-    return (
-      <li>
-        <p className="bold red">
-          {con}
-        </p>
-      </li>
-    )
-  }));
+  const [conElems, setConElems] = useState(
+    cons.map((con, index) => {
+      return (
+        <li key={index}>
+          <p className="bold red">{con}</p>
+        </li>
+      );
+    })
+  );
 
   return (
-   <Cont colors = {COLORS} className = 'supplier-box' data-aos="fade-up">
-    <div className="text-box flex-one">
-      <div className="center-inline">
-      <h3 className="black mar-bottom-16">{name}</h3>
+    <Cont colors={COLORS} className="supplier-box" data-aos="fade-up">
+      <div className="text-box flex-one">
+        <div className="center-inline">
+          <h3 className="black mar-bottom-16">{name}</h3>
+        </div>
+        <div className="lines">
+          <div className="flex align-center">
+            <FontAwesomeIcon
+              icon={faLink}
+              className="light-blue icon-sm mar-right-8"
+            />
+            <Link href={links[0]}>
+              <p className="bold"> {links[0]} </p>
+            </Link>
+          </div>
+          {links[1] !== null && (
+            <div className="flex align-center">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="mar-right-8 light-blue icon-sm"
+              />
+              <Link href={links[1]}>
+                <p className="bold"> {links[1]} </p>
+              </Link>
+            </div>
+          )}
+
+          <ul className="flex align-center flex-column">{proElems}</ul>
+          <ul className="flex-align-center flex-column">{conElems}</ul>
+        </div>
       </div>
-      <div className="lines">
-      <div className = 'flex align-center'>
-        <FontAwesomeIcon icon = {faLink} className = 'light-blue icon-sm mar-right-8' />
-        <Link href = {links[0]}>
-          <p className = 'bold'> {links[0]} </p> 
-          </Link>
-        </div>
-        {links[1] !== null && (
-        <div  className = 'flex align-center'>
-        <FontAwesomeIcon icon = {faInstagram} className = 'mar-right-8 light-blue icon-sm' />
-        <Link href = {links[1]}>
-          <p className = 'bold'> {links[1]} </p> 
-          </Link>
-        </div>)
-}
+      <div className="image-holder flex-one">
+        <Image
+          quality="100"
+          fill
+          src={image}
+          style={{ objectFit: "contain" }}
+        />
+      </div>
+    </Cont>
+  );
+};
 
-        <ul className = 'flex align-center flex-column'>
-          {proElems}
-        </ul>
-        <ul className="flex-align-center flex-column">
-          {conElems}
-        </ul>
-        </div>
-    </div> 
-    <div className="image-holder flex-one">
-      
-    <Image 
-      quality= '100'
-      fill
-      src = {image}
-      style = {{objectFit:'contain'}}
-      />
-    </div>
-    
-   </Cont>
-  )
-}
-
-export default SupplierBox
+export default SupplierBox;
