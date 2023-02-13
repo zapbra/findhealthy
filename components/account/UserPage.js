f97d835682a29632e5798dbeb9d3197148ae038e
javascriptismything <125082417+javascriptismything@users.noreply.github.com>
2023-02-13 21:51:02 +0000

    c

--- components/account/UserPage.js
+++ components/account/UserPage.js
@@ -1,9 +1,18 @@
 import styled from "styled-components";
 import COLORS from "../../data/colors";
-const Cont = styled.div``;
+const Cont = styled.div`
+  .default-page {
+    background: #fff;
+    border: 1px solid ${(props) => props.colors.grey};
+  }
+`;
 
 const UserPage = () => {
-  return <Cont color={COLORS}></Cont>;
+  return (
+    <Cont colors={COLORS}>
+      <div className="default-page"></div>
+    </Cont>
+  );
 };
 
 export default UserPage;
