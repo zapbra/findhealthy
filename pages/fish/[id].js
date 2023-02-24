1f3d922e54ff07c5fe3f7d8de2b2b181356763fc
javascriptornah <125489002+javascriptornah@users.noreply.github.com>
2023-02-24 13:28:25 +0000

    c

--- pages/fish/[id].js
+++ pages/fish/[id].js
@@ -1,5 +1,10 @@
 import { useState } from "react";
+import styled from "styled-components";
 import COLORS from '../../data/colors';
+
+const Cont = styled.div`
+
+`;
 
 const Fish = ({}) => {
     return (
