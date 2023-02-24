fba7f6db50c6e716b7ebab2d540854f912ada3e0
javascriptornah <125489002+javascriptornah@users.noreply.github.com>
2023-02-24 13:27:19 +0000

    c

---
+++ pages/fish/[id].js
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import COLORS from '../../data/colors';
+
+const Fish = ({}) => {
+    return (
+        <Cont colors = {COLORS}></Cont>
+    )
+}
+
+export default Fish;
