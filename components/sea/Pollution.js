0958afef045343f6976348c64c3e3fcd485d5cf4
javascriptornah <125489002+javascriptornah@users.noreply.github.com>
2023-03-01 15:46:27 +0000

    c

--- components/sea/Pollution.js
+++ components/sea/Pollution.js
@@ -1,10 +1,63 @@
 import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
-const Cont = styled.div``;
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar, faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import ReactMarkdown from "react-markdown";
+
+const Cont = styled.div`
+  border: 1px solid ${(props) => props.colors.black};
+  border-radius: 8px;
+  padding: 8px;
+  text-align: center;
+
+  margin: 0 auto;
+  .text-content {
+    overflow: hidden;
+    padding: 16px;
+  }
+`;
 
 const Pollution = ({ pollution }) => {
-  return <Cont colors={COLORS}></Cont>;
+  const [visible, setVisible] = useState(false);
+  const toggleVisible = () => {
+    setVisible(!visible);
+  };
+  return (
+    <Cont
+      colors={COLORS}
+      className="mar-bottom-32"
+      style={{ maxWidth: visible ? "1200px" : "400px" }}
+    >
+      <h4 className="black mar-bottom-16">{pollution.name}</h4>
+      <div className="star-holder mar-bottom-16">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((index, realIndex) => {
+          return (
+            <FontAwesomeIcon
+              key={realIndex}
+              icon={faStar}
+              className={
+                index <= pollution.severity
+                  ? "icon-ssm light-red"
+                  : "icon-ssm black"
+              }
+            />
+          );
+        })}
+      </div>
+      <div
+        className="text-content"
+        style={{ maxHeight: visible ? "none" : "320px" }}
+      >
+        <ReactMarkdown classname="markdown">
+          {pollution.description}
+        </ReactMarkdown>
+      </div>
+      <div className="green-icon box-shadow-2" onClick={toggleVisible}>
+        <FontAwesomeIcon icon={faChevronDown} className="white icon-ssm" />
+      </div>
+    </Cont>
+  );
 };
 
 export default Pollution;
--- components/sea/PollutionHolder.js
+++ components/sea/PollutionHolder.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
+import Pollution from "./Pollution";
 const Cont = styled.div``;
 
-const PollutionHolder = ({ PollutionEvents }) => {
+const PollutionHolder = ({ pollutionEvents }) => {
+  const pollutionElems = pollutionEvents.map((pollution) => {
+    return <Pollution pollution={pollution} />;
+  });
   return (
     <Cont colors={COLORS}>
       <div className="flex justify-center">
-        <div className="header-2 box-shadow-2">
+        <div className="header-2 box-shadow-2 mar-bottom-32 center-inline">
           <h3 className="white">How Polluted Is It?</h3>
         </div>
       </div>
+      <div>{pollutionElems}</div>
     </Cont>
   );
 };
