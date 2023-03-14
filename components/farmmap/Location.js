c669fce03c3435198eb36e570c3ba7ee751c7549
javascriptornah <125489002+javascriptornah@users.noreply.github.com>
2023-03-14 20:47:31 +0000

    c

---
+++ components/farmmap/Location.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import styled from "styled-components";
+import COLORS from "../../data/colors";
+
+const Cont = styled.div``;
+
+const Location = () => {
+  return <Cont colors={COLORS}></Cont>;
+};
+
+export default Location;
--- pages/farmmap.js
+++ pages/farmmap.js
@@ -2,13 +2,17 @@ import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../data/colors";
 
-const Cont = styled.div``;
+const Cont = styled.div`
+  margin-top: 48px;
+`;
 
 const Farmmap = () => {
   return (
     <Cont colors={COLORS}>
-      <div className="header-3">
-        <h4>FIND FARMS NEAR YOU</h4>
+      <div className="center-inline">
+        <div className="header-3">
+          <h4>FIND FARMS NEAR YOU</h4>
+        </div>
       </div>
     </Cont>
   );
