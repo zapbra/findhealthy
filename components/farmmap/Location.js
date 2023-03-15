f739414e0f13bc6cf8f9e6530b3841de83d9d4af
javascriptornah <125489002+javascriptornah@users.noreply.github.com>
2023-03-15 11:49:11 +0000

    c

--- components/farmmap/Location.js
+++ components/farmmap/Location.js
@@ -2,7 +2,33 @@ import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
 import Select from "../google/Select";
-const Cont = styled.div``;
+const Cont = styled.div`
+  display: inline-block;
+  .dropdown {
+    border: none;
+    &__menu {
+      border: 1px solid ${(props) => props.colors.grey};
+    }
+    &__menu_search {
+      border: 1px solid ${(props) => props.colors.darkBlue};
+    }
+  }
+  .dropdown__selected {
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px,
+      rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+    border-radius: 8px;
+    &:hover {
+      border: 1px solid ${(props) => props.colors.black};
+    }
+  }
+  .select-line {
+    margin-right: 32px;
+    @media only screen and (max-width: 400px) {
+      margin-right: 0;
+    }
+  }
+`;
 
 const Location = ({
   countries,
@@ -19,9 +45,9 @@ const Location = ({
 }) => {
   return (
     <Cont colors={COLORS}>
-      <h5 className="mar-bottom-32-res">LOCATION</h5>
-      <div className="tan-container flex">
-        <div className="mar-right-32">
+      <h5 className="mar-bottom-16">LOCATION</h5>
+      <div className="tan-container flex flex-wrap justify-center">
+        <div className="select-line mar-bottom-16">
           <h5 className="light contrast mar-bottom-16">COUNTRY</h5>
           <Select
             regions={countries}
@@ -33,7 +59,7 @@ const Location = ({
             name="country"
           />
         </div>
-        <div className="mar-right-32">
+        <div className="select-line mar-bottom-16">
           <h5 className="light contrast mar-bottom-16">STATE</h5>
           <Select
             regions={states}
@@ -44,7 +70,7 @@ const Location = ({
             name="state"
           />
         </div>
-        <div className="mar-right-32">
+        <div className="select-line mar-bottom-16">
           <h5 className="light contrast mar-bottom-16">CITY</h5>
           <Select
             regions={cities}
