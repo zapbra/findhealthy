33eee63fc984628a978e42c84fe48a47de790d24
javascriptornah <125489002+javascriptornah@users.noreply.github.com>
2023-03-15 11:43:07 +0000

    c

--- components/farmmap/Location.js
+++ components/farmmap/Location.js
@@ -1,11 +1,64 @@
 import { useState } from "react";
 import styled from "styled-components";
 import COLORS from "../../data/colors";
-
+import Select from "../google/Select";
 const Cont = styled.div``;
 
-const Location = () => {
-  return <Cont colors={COLORS}></Cont>;
+const Location = ({
+  countries,
+  country,
+  updateRegion,
+  options,
+  setOptions,
+  states,
+  state,
+  setStates,
+  city,
+  cities,
+  setCities,
+}) => {
+  return (
+    <Cont colors={COLORS}>
+      <h5 className="mar-bottom-32-res">LOCATION</h5>
+      <div className="tan-container flex">
+        <div className="mar-right-32">
+          <h5 className="light contrast mar-bottom-16">COUNTRY</h5>
+          <Select
+            regions={countries}
+            value={country}
+            updateValue={updateRegion}
+            searchPlaceholder="Search"
+            options={options}
+            setOptions={setOptions}
+            name="country"
+          />
+        </div>
+        <div className="mar-right-32">
+          <h5 className="light contrast mar-bottom-16">STATE</h5>
+          <Select
+            regions={states}
+            value={state}
+            updateValue={updateRegion}
+            options={states}
+            setOptions={setStates}
+            name="state"
+          />
+        </div>
+        <div className="mar-right-32">
+          <h5 className="light contrast mar-bottom-16">CITY</h5>
+          <Select
+            regions={cities}
+            value={city}
+            updateValue={updateRegion}
+            searchPlaceholder="Search"
+            options={cities}
+            setOptions={setCities}
+            name="city"
+          />
+        </div>
+      </div>
+    </Cont>
+  );
 };
 
 export default Location;
--- pages/farmmap.js
+++ pages/farmmap.js
@@ -1,18 +1,154 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import COLORS from "../data/colors";
-
+import Location from "../components/farmmap/Location";
+import axios from "axios";
 const Cont = styled.div`
   margin-top: 48px;
 `;
 
 const Farmmap = () => {
+  const [country, setCountry] = useState("");
+  const [state, setState] = useState("");
+  const [city, setCity] = useState("");
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+  const [data, setData] = useState([]);
+  const [locations, setLocations] = useState([]);
+  const [options, setOptions] = useState([]);
+  const [regions, setRegions] = useState([]);
+  const [foodLocations, setFoodLocaitons] = useState([]);
+  const changeHandler = (value, name) => {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: {
+        value,
+        error: value !== "" ? "" : prev[name].error,
+      },
+    }));
+    if (name === "country") {
+      updateCountry(value);
+    } else if (name === "state") {
+      updateState(value);
+    } else if (name === "city") {
+      updateCity(value);
+    }
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://pkgstore.datahub.io/core/world-cities/world-cities_json/data/5b3dd46ad10990bca47b04b4739a02ba/world-cities_json.json"
+      )
+      .then((res) => {
+        setData((prevData) => {
+          return res.data;
+        });
+        setRegions((prevData) => {
+          return [...new Set(res.data.map((item) => item.country))].sort();
+        });
+        setOptions((prevData) => {
+          return [...new Set(res.data.map((item) => item.country))].sort();
+        });
+      })
+      .catch((err) => console.log(err));
+
+    setLocations((prevLocations) => {
+      let newLocations = foodLocations.sort((a, b) => {
+        return a.title.localeCompare(b.title);
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    setLocations((prevLocations) => {
+      return foodLocations.filter((location) => {
+        return location.country.title === country;
+      });
+    });
+  }, [country]);
+
+  useEffect(() => {
+    setLocations((prevLocations) => {
+      return foodLocations.filter((location) => {
+        return location.state.title === state;
+      });
+    });
+  }, [state]);
+
+  useEffect(() => {
+    setLocations((prevLocations) => {
+      return foodLocations.filter((location) => {
+        return location.city.title === city;
+      });
+    });
+  }, [city]);
+
+  function updateCountry(value) {
+    console.log(value);
+    setCountry((prevCountry) => {
+      return value;
+    });
+    let states = data.filter((item) => {
+      return item.country === value;
+    });
+    states = [...new Set(states.map((item) => item.subcountry))];
+    states.sort();
+    setStates((prevStates) => {
+      return states;
+    });
+    setState("?");
+    setCity("");
+  }
+
+  function updateState(value) {
+    setState((prevState) => {
+      return value;
+    });
+    let cities = data.filter((city) => city.subcountry === value);
+    cities = cities.map((city) => city.name);
+    cities.sort();
+    setCities(cities);
+  }
+
+  function updateCity(value) {
+    setCity((prevCity) => {
+      return value;
+    });
+  }
+
+  function updateRegion(location, name) {
+    if (name === "country") {
+      updateCountry(location);
+    } else if (name === "state") {
+      updateState(location);
+    } else if (name == "city") {
+      updateCity(location);
+    }
+  }
+
   return (
     <Cont colors={COLORS}>
       <div className="center-inline">
         <div className="header-3">
           <h4>FIND FARMS NEAR YOU</h4>
         </div>
+      </div>
+      <div>
+        <Location
+          countries={regions}
+          country={country}
+          updateRegion={updateRegion}
+          options={options}
+          setOptions={setOptions}
+          states={states}
+          state={state}
+          setStates={setStates}
+          cities={cities}
+          city={city}
+          setCities={setCities}
+        />
       </div>
     </Cont>
   );
